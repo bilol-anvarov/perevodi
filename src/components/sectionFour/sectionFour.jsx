4e25e068f776e118d6526f8ec066c528045c553a
bilol-anvarov <bilolanvarov8080@gmail.com>
2024-10-21 12:14:29 +0500

    its the last commit

--- src/components/sectionFour/sectionFour.jsx
+++ src/components/sectionFour/sectionFour.jsx
@@ -29,7 +29,6 @@ const images = [
     '/toys 18.png',
     '/toys 19.png',
     '/toys 20.png',
-    '/toys 21.png',
     '/toys 22.png',
     '/toys 23.png',
     '/toys 24.png',
@@ -112,112 +111,8 @@ export default function SectionFour() {
                     </div>
                 )
             })}
-            {/* <div className="imgCtr">
-                <img
-                src={'/1 (2).png'}
-                alt={`Slide`}
-                className="slider-image"
-                />
-            </div>
-            <div className="imgCtr">
-                <img
-                src={'/2 (2).png'}
-                alt={`Slide`}
-                className="slider-image"
-                />
-            </div>
-            <div className="imgCtr">
-                <img
-                src={'/3 (2).png'}
-                alt={`Slide`}
-                className="slider-image"
-                />
-            </div>
-            <div className="imgCtr">
-                <img
-                src={'/4 (2).png'}
-                alt={`Slide`}
-                className="slider-image"
-                />
-            </div>
-            <div className="imgCtr">
-                <img
-                src={'/5.png'}
-                alt={`Slide`}
-                className="slider-image"
-                />
-            </div> */}
-
-
         </Slider>
         </div>
     </div>
   );
 }
-// export default function SectionFour() {
-//     const settings = {
-//         dots: true,
-//         infinite: true,
-//         speed: 2000,
-//         arrows: false,              
-//         pauseOnHover: true,    
-//         dots: false,
-//         slidesToShow: 10,
-//         slidesToScroll: 1,
-//         cssEase: 'linear',
-//         autoplay: true,
-//         autoplaySpeed: 0,
-//         variableWidth: true,
-//         draggable: true,
-//         rtl: false,
-//         touchThreshold: 15,         // Более высокое значение делает обратный свайп труднее
-//         swipeToSlide: true 
-//       };
-    
-//   const { t } = useTranslation();
-  
-//     return (
-//       <div className="scroll-container fade-in-section opacity-0    ">
-//         <h2 className="title">{t('mainPage.sectionFive.title')}</h2>
-//         <div className="scroll-content">
-//             <Slider {...settings}>
-
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/1 (2).png'}/>
-//                 </div>
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/2 (2).png'}/>
-//                 </div>
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/3 (2).png'}/>
-//                 </div>
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/4 (2).png'}/>
-//                 </div>
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/5.png'}/>
-//                 </div>
-
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/1 (2).png'}/>
-//                 </div>
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/2 (2).png'}/>
-//                 </div>
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/3 (2).png'}/>
-//                 </div>
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/4 (2).png'}/>
-//                 </div>
-//                 <div className="sliderImgCtr">
-//                     <MyImg src={'/5.png'}/>
-//                 </div>
-
-
-//             </Slider>
-          
-//         </div>
-//       </div>
-//     );
-// }
