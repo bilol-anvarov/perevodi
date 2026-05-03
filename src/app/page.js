4e14ba41f3011ffa447889fd5737d00b13756d67
Bilol Anvarov <Bilolkhoja_Anvarov@student.itpu.uz>
2026-05-03 03:41:01 -0500

    upd

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import Hero from "@/components/MainPage sections/Hero";
+import Hero from "@/components/MainPage/Hero";
 import { useTranslation } from "next-i18next";
 
 export default function Home() {
