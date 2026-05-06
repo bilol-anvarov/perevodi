fa65db83d88dcc8a471daf489481268f2d864168
Bilol Anvarov <Bilolkhoja_Anvarov@student.itpu.uz>
2026-05-05 20:40:45 -0500

    update sections and footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ru">
+    <html lang="uz">
       <body>
         <MainContextProvider>
           <Header />
