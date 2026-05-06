41f9f326e27ddd470c154636e89cfce8c68c5ed1
Bilol Anvarov <Bilolkhoja_Anvarov@student.itpu.uz>
2026-05-05 21:03:24 -0500

    upd

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+"use client";
+
+import ContactForm from "@/components/MainPage/Contactform";
+import Hero from "@/components/MainPage/Hero";
+import Services from "@/components/MainPage/Services";
+import WhyUs from "@/components/MainPage/Whyus";
+import { useTranslation } from "next-i18next";
+
+export default function Home() {
+  const { t } = useTranslation();
+
+  return (
+    <main className="">
+      <Hero />
+      <Services />
+      <WhyUs />
+      <ContactForm />
+    </main>
+  );
+}
